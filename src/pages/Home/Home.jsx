8b1fe1503cdc9f8ad0f6f80d9352c8c5f6ffdf9b
Joshua <joshua.edo01@gmail.com>
2023-02-12 23:26:10 +0100

    add page--background class

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,9 @@
-import React, { Component } from "react";
 import Hero from "./Hero";
 
-export default class Home extends Component {
-  render() {
-    return (
-      <>
-        <Hero />
-      </>
-    );
-  }
+export default function Home() {
+  return (
+    <div className="Home page--background">
+      <Hero />
+    </div>
+  )
 }
