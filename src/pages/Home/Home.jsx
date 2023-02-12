de88ee4ccefd5ec24ffe13619024be6c13d6622d
Joshua <joshua.edo01@gmail.com>
2023-02-12 22:59:47 +0100

    remove contact, project, stacks frome home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,11 @@
 import React, { Component } from "react";
 import Hero from "./Hero";
-import Stacks from "../Stacks/Stacks";
-import Projects from "../Projects/Projects";
-import Contact from "../Contact/Contact";
 
 export default class Home extends Component {
   render() {
     return (
       <>
         <Hero />
-        {/* <Stacks />
-        <Projects />
-        <Contact /> */}
       </>
     );
   }
