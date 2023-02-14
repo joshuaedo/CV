c71771f5de7ccdb312aaa45159bf07e9523aa7af
Joshua Edo <joshua.edo01@gmail.com>
2023-02-14 22:44:03 +0000

    add contact page

---
+++ src/pages/Contact/content_option.js
@@ -0,0 +1,11 @@
+const contactConfig = {
+  YOUR_EMAIL: "joshua.edo01@gmail.com",
+  YOUR_FONE: "(+234)810-733-9203",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+export { contactConfig };
