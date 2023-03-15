7ad9efa073e4c887b252ff763ec35024929a7c5d
Joshua Edo <joshua.edo01@gmail.com>
2023-03-15 20:29:37 +0000

    fix mapping

--- src/pages/Projects/cardData.js
+++ src/pages/Projects/cardData.js
@@ -1,21 +1,26 @@
+import Arese from "./images/arese.png";
+
 const cardData = [
   {
     id: 1,
-    img: { src: require("./images/arese.png"), alt: "Arese Sylvester - Home" },
+    img: {
+      src: Arese,
+      alt: "Arese Sylvester - Home",
+    },
     title: "Arese Sylvester",
     description: "The portfolio website for Arese sylvester",
     href: "https://www.aresesylvester.com/",
   },
   {
     id: 2,
-    img: { src: require("./images/ryter.png"), alt: "Ryter - Home " },
+    img: { src: require("./images/ryter.png").default, alt: "Ryter - Home " },
     title: "Ryter",
     description: "A productivity app for writers",
     href: "https://ryter.netlify.app/",
   },
   {
     id: 3,
-    img: { src: require("./images/cleander.png"), alt: "Cleander" },
+    img: { src: require("./images/cleander.png").default, alt: "Cleander" },
     title: "Cleander",
     description: "A landing page for Cleander",
     href: "https://thecleander.netlify.app/",
@@ -23,7 +28,7 @@ const cardData = [
   {
     id: 4,
     img: {
-      src: require("./images/business-card.png"),
+      src: require("./images/business-card.png").default,
       alt: "Joshua Edo's Business Card",
     },
     title: "Business Card",
@@ -32,21 +37,24 @@ const cardData = [
   },
   {
     id: 5,
-    img: { src: require("./images/drum.png"), alt: "Drum Kit" },
+    img: { src: require("./images/drum.png").default, alt: "Drum Kit" },
     title: "Drum Kit",
     description: "A drum kit",
     href: "https://josh-drum.netlify.app/",
   },
   {
     id: 6,
-    img: { src: require("./images/dice.png"), alt: "Dice Game" },
+    img: { src: require("./images/dice.png").default, alt: "Dice Game" },
     title: "Dice Game",
     description: "A simple dice game",
     href: "https://thesimpledicegame.netlify.app/",
   },
   {
     id: 7,
-    img: { src: require("./images/portfolio.png"), alt: "Joshua Edo - Home" },
+    img: {
+      src: require("./images/portfolio.png").default,
+      alt: "Joshua Edo - Home",
+    },
     title: "CV",
     description: "My first portfolio website",
     href: "https://joshuaedo-cv2.netlify.app/",
@@ -60,4 +68,4 @@ const cardData = [
   // },
 ];
 
-export default cardData;
+export { cardData };
