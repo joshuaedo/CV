7f032b46431d1a5f26d2040b6f3cc1df186f03ce
Joshua Edo <joshua.edo01@gmail.com>
2023-03-16 14:47:48 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
-import { Html, Head, Main, NextScript, Script } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 import { Container } from "react-bootstrap";
+import Script from "next/script";
 
 export default function Document() {
   return (
