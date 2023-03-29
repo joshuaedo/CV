f2d09acb859378947dbb775306aa99bf77e8dce2
Joshua <joshua.edo01@gmail.com>
2023-03-29 21:01:13 +0100

    add google project

--- pages/projects.js
+++ pages/projects.js
@@ -1,22 +1,22 @@
-import Card from "@/components/Card";
-import { cardData } from "@/lib/cardData";
-import Head from "next/head";
-import Navbar from "@/components/Navbar";
+import Card from '@/components/Card';
+import { cardData } from '@/lib/cardData';
+import Head from 'next/head';
+import Navbar from '@/components/Navbar';
 
 export default function Projects() {
   return (
     <>
       <Head>
         <title>Joshua Edo - Projects</title>
-        <meta name="description" content="Joshua Edo's Projects" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='description' content="Joshua Edo's Projects" />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <Navbar />
-      <div className="Projects">
-        <div className="row">
+      <div className='Projects'>
+        <div className='row'>
           {cardData.map((props) => (
             <Card
-              key={props.id}
+              key={props.title}
               src={props.img.src}
               alt={props.img.alt}
               title={props.title}
