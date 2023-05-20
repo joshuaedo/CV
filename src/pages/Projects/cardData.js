bd5524d7c19c844fa3dc24abe34df0fd5f697435
Joshua Edo <joshua.edo01@gmail.com>
2023-05-20 22:23:11 +0000

    Update cardData.js

--- src/pages/Projects/cardData.js
+++ src/pages/Projects/cardData.js
@@ -1,108 +1,108 @@
-import Spotify from './images/spotify.png';
-import Netflix from './images/netflix.png';
-import Arese from './images/arese.png';
-import Blog from './images/blog.png';
-import Ryter from './images/ryter.png';
-import Cleander from './images/cleander.png';
-import BusinessCard from './images/business-card.png';
-import DrumKit from './images/drum.png';
-import DiceGame from './images/dice.png';
-import StyleYourDiv from './images/style-your-div.png';
-import CV from './images/portfolio.png';
-import Google from './images/google.png';
-import joshuaGPT from './images/joshuaGPT.png';
+import Spotify from "./images/spotify.png";
+import Netflix from "./images/netflix.png";
+// import Arese from './images/arese.png';
+import Blog from "./images/blog.png";
+import Ryter from "./images/ryter.png";
+import Cleander from "./images/cleander.png";
+import BusinessCard from "./images/business-card.png";
+import DrumKit from "./images/drum.png";
+import DiceGame from "./images/dice.png";
+import StyleYourDiv from "./images/style-your-div.png";
+import CV from "./images/portfolio.png";
+import Google from "./images/google.png";
+import joshuaGPT from "./images/joshuaGPT.png";
 // import  from "@/assets/images/.png";
 
 const cardData = [
   {
-    img: { src: Spotify, alt: 'Spotify Web App Clone' },
-    title: 'Spotify Clone',
-    description: 'A clone of the spotify web app, built with NextJs',
-    href: 'https://joshuaedo-spotify.vercel.app/',
+    img: { src: Spotify, alt: "Spotify Web App Clone" },
+    title: "Spotify Clone",
+    description: "A clone of the spotify web app, built with NextJs",
+    href: "https://joshuaedo-spotify.vercel.app/",
   },
   {
-    img: { src: joshuaGPT, alt: 'joshuaGPT' },
-    title: 'joshuaGPT',
+    img: { src: joshuaGPT, alt: "joshuaGPT" },
+    title: "joshuaGPT",
     description: `A Telegram Bot built with Node.js and powered by OpenAI's chatGPT model`,
-    href: 'https://t.me/joshuaedo_bot',
+    href: "https://t.me/joshuaedo_bot",
   },
   {
-    img: { src: Netflix, alt: 'Netflix Clone' },
-    title: 'Netflix Clone',
-    description: 'A clone of netflix built with React and the TMDB API',
-    href: 'https://joshuaedo-netflix.web.app/',
+    img: { src: Netflix, alt: "Netflix Clone" },
+    title: "Netflix Clone",
+    description: "A clone of netflix built with React and the TMDB API",
+    href: "https://joshuaedo-netflix.web.app/",
   },
-  {
-    img: {
-      src: Arese,
-      alt: 'Arese Sylvester - Home',
-    },
-    title: 'Arese Sylvester',
-    description: 'The portfolio website for Arese sylvester',
-    href: 'https://www.aresesylvester.com/',
-  },
+  // {
+  //   img: {
+  //     src: Arese,
+  //     alt: 'Arese Sylvester - Home',
+  //   },
+  //   title: 'Arese Sylvester',
+  //   description: 'The portfolio website for Arese sylvester',
+  //   href: 'https://www.aresesylvester.com/',
+  // },
   {
     img: { src: Blog, alt: "Joshua Edo's Blog" },
-    title: 'My Blog',
-    description: 'A blog website built with GraphQL and NextJs',
-    href: 'https://joshuaedo-blog.vercel.app/',
+    title: "My Blog",
+    description: "A blog website built with GraphQL and NextJs",
+    href: "https://joshuaedo-blog.vercel.app/",
   },
   {
-    img: { src: Ryter, alt: 'Ryter - Home ' },
-    title: 'Ryter',
-    description: 'A productivity app for writers',
-    href: 'https://ryter.netlify.app/',
+    img: { src: Ryter, alt: "Ryter - Home " },
+    title: "Ryter",
+    description: "A productivity app for writers",
+    href: "https://ryter.netlify.app/",
   },
   {
-    img: { src: Google, alt: 'Google Search Engine' },
-    title: 'Google',
+    img: { src: Google, alt: "Google Search Engine" },
+    title: "Google",
     description: `A search engine made with Google's API and NextJs`,
-    href: 'https://joshuaedo-google.vercel.app/',
+    href: "https://joshuaedo-google.vercel.app/",
   },
   {
-    img: { src: Cleander, alt: 'Cleander' },
-    title: 'Cleander',
-    description: 'A landing page for Cleander',
-    href: 'https://thecleander.netlify.app/',
+    img: { src: Cleander, alt: "Cleander" },
+    title: "Cleander",
+    description: "A landing page for Cleander",
+    href: "https://thecleander.netlify.app/",
   },
   {
     img: {
       src: BusinessCard,
       alt: "Joshua Edo's Business Card",
     },
-    title: 'Business Card',
-    description: 'A business card made with react',
-    href: 'https://joshuaedocard.netlify.app/',
+    title: "Business Card",
+    description: "A business card made with react",
+    href: "https://joshuaedocard.netlify.app/",
   },
   {
-    img: { src: DrumKit, alt: 'Drum Kit' },
-    title: 'Drum Kit',
-    description: 'A drum kit',
-    href: 'https://josh-drum.netlify.app/',
+    img: { src: DrumKit, alt: "Drum Kit" },
+    title: "Drum Kit",
+    description: "A drum kit",
+    href: "https://josh-drum.netlify.app/",
   },
   {
-    img: { src: DiceGame, alt: 'Dice Game' },
-    title: 'Dice Game',
-    description: 'A simple dice game',
-    href: 'https://thesimpledicegame.netlify.app/',
+    img: { src: DiceGame, alt: "Dice Game" },
+    title: "Dice Game",
+    description: "A simple dice game",
+    href: "https://thesimpledicegame.netlify.app/",
   },
   {
     img: {
       src: StyleYourDiv,
-      alt: 'Style Your Div',
+      alt: "Style Your Div",
     },
-    title: 'Style Your Div',
-    description: 'Style a Div by editing text directly in the DOM',
-    href: 'https://style-your-div.netlify.app/',
+    title: "Style Your Div",
+    description: "Style a Div by editing text directly in the DOM",
+    href: "https://style-your-div.netlify.app/",
   },
   {
     img: {
       src: CV,
-      alt: 'Joshua Edo - Home',
+      alt: "Joshua Edo - Home",
     },
-    title: 'CV',
-    description: 'My first portfolio website',
-    href: 'https://joshuaedo-cv2.netlify.app/',
+    title: "CV",
+    description: "My first portfolio website",
+    href: "https://joshuaedo-cv2.netlify.app/",
   },
 ];
 
