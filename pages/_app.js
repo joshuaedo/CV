7efdc58f3d4f176d5546aaefb48e02dded07038d
Joshua Edo <joshua.edo01@gmail.com>
2023-06-28 09:07:47 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.css";
 import "@/styles/Navbar.css";
@@ -5,22 +6,22 @@ import "@/styles/Home.css";
 import "@/styles/Card.css";
 import "@/styles/Contact.css";
 import "@/styles/Stacks.css";
-import Script from "next/script";
+// import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       {/* Scripts */}
-      <Script
+      <script
         src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossOrigin="anonymous"
-      ></Script>
-      <Script
+      ></script>
+      <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
         crossOrigin="anonymous"
-      ></Script>
+      ></script>
       <Component {...pageProps} />
     </>
   );
